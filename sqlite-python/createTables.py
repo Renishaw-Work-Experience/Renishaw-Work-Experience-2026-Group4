d3eac0670a0151d19b6c09dac690743aea52fe29
leonhard-euler-2718 <archienassif@icloud.com>
2026-07-13 15:00:31 +0100

    Setup database

---
+++ sqlite-python/createTables.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+sql_statements = [ 
+    """CREATE TABLE IF NOT EXISTS Accounts (
+            accountID INTEGER PRIMARY KEY, 
+            username text NOT NULL, 
+            password text NOT NULL
+        );
+        """,
+    """
+        CREATE TABLE IF NOT EXISTS Messages (
+            messageID INTEGER PRIMARY KEY,
+            senderID INTEGER NOT NULL,
+            chatRoomID INTEGER NOT NULL,
+            content text NOT NULL,
+            TimeSent DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            FOREIGN KEY (senderID) REFERENCES Accounts(accountID),
+            FOREIGN KEY (chatRoomID) REFERENCES ChatRooms(roomID)
+        );
+        """,
+    """
+        CREATE TABLE IF NOT EXISTS ChatRooms (
+            roomID INTEGER PRIMARY KEY,
+            name text NOT NULL,
+            user1ID INTEGER NOT NULL,
+            user2ID INTEGER NOT NULL,
+            user3ID INTEGER,
+            user4ID INTEGER,
+            user5ID INTEGER,
+            FOREIGN KEY (user1ID) REFERENCES Accounts(AccountID),
+            FOREIGN KEY (user2ID) REFERENCES Accounts(AccountID),
+            FOREIGN KEY (user3ID) REFERENCES Accounts(AccountID),
+            FOREIGN KEY (user4ID) REFERENCES Accounts(AccountID),
+            FOREIGN KEY (user5ID) REFERENCES Accounts(AccountID)
+        );""",
+]
+
+# Create an account:
+#   INSERT INTO Accounts (Username, Password) VALUES ('text', 'text');
+
+# create a database connection
+try:
+    with sqlite3.connect('my.db') as conn:
+        # create a cursor
+        cursor = conn.cursor()
+
+        # execute statements
+        for statement in sql_statements:
+            cursor.execute(statement)
+
+        # commit the changes
+        conn.commit()
+
+        print("Tables created successfully.")
+except sqlite3.OperationalError as e:
+    print("Failed to create tables:", e)

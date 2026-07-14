7b9a455bd838db8edb8626019327fd48e7e877db
Jedd <jedd@ashmeadsolutions.co.uk>
2026-07-14 11:28:37 +0100

    fixed file paths for images

--- GUI/chatinterface2.py
+++ GUI/chatinterface2.py
@@ -1,5 +1,9 @@
 import customtkinter 
 from PIL import Image
+import os
+
+# ensure image paths work regardless of current working directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 app = customtkinter.CTk()
 app.geometry("1000x600")
@@ -23,9 +27,9 @@ chatnameframe.place(relx=0.27, rely=0.03, relwidth= 0.71, relheight=0.10, anchor
 app.grid_columnconfigure(0,weight=1)
 app.grid_rowconfigure(0,weight=1)
 
-sendimage = customtkinter.CTkImage(light_image=Image.open("4008617.png"), size=(50,50))
+sendimage = customtkinter.CTkImage(light_image=Image.open(os.path.join(script_dir, "send_button.png")), size=(50,50))
 
-newchatimage = customtkinter.CTkImage(light_image=Image.open("13122628.png"), size=(50,50))
+newchatimage = customtkinter.CTkImage(light_image=Image.open(os.path.join(script_dir, "new_chat.png")), size=(50,50))
 newchatlabel = customtkinter.CTkButton(chatroomsframe, image=newchatimage, text="", corner_radius=50, fg_color="deep sky blue")
 newchatlabel.place(relx=0.6,rely=0.22, relwidth=0.3, relheight=0.5)
 
@@ -39,7 +43,7 @@ chatcontent.place(relx=0.27,rely=0.15, relwidth=0.71, relheight=0.65)
 messageinputbox = customtkinter.CTkTextbox(app, fg_color="grey60", corner_radius=20)
 messageinputbox.place(relx=0.27, rely=0.82, relwidth=0.6, relheight=0.15)
 
-sendimage = customtkinter.CTkImage(light_image=Image.open("4008617.png"), size=(50,50))
+sendimage = customtkinter.CTkImage(light_image=Image.open(os.path.join(script_dir, "new_chat.png")), size=(50,50))
 sendbtn = customtkinter.CTkButton(app, image=sendimage, text="", fg_color="deep sky blue", corner_radius=50)
 sendbtn.place(relx=0.89, rely=0.84, relwidth=0.07, relheight=0.11)
 
